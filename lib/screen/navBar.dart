625f84c5255f02da15585c7b183a6454444d9098
Dragonite24 <qwez-9@mail.ru>
2020-12-18 18:41:14 +0300

    Это не надо)

--- lib/screen/navBar.dart
+++ lib/screen/navBar.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_in/screen/home.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bottom Navigation Bar',
-      home: MyHomePage(),
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
 
