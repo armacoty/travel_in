b3c8822d78f70e05f8090ee54b411e9f07937119
Vinogradov Alexander <astronom_35@mail.ru>
2020-12-18 19:59:11 +0300

    Update prear.dart

--- lib/screen/prear.dart
+++ lib/screen/prear.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:travel_in/screen/ar.dart';
 
 class PreARPage extends StatefulWidget {
   @override
@@ -8,15 +10,25 @@ class PreARPage extends StatefulWidget {
 class _PreARPageState extends State<PreARPage> {
   @override
   Widget build(BuildContext context) {
+    postLoad();
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Opening camera"),
-      ),
       body: Center(
         child: Text(
-          'Opening camera',
+          "Открытие камеры",
+          style: TextStyle(color: Colors.white),
         ),
       ),
+      backgroundColor: Colors.black,
     );
+  }
+
+  void postLoad() async {
+    if (await Permission.microphone.request().isGranted) {
+      if (await Permission.camera.request().isGranted) {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (BuildContext context) => ARPage(),
+        ));
+      }
+    }
   }
 }
