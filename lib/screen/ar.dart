2ccf02a7f1ad1c5ed81f01a62db74bdfac190673
Vinogradov Alexander <astronom_35@mail.ru>
2020-12-18 22:41:51 +0300

    tts

--- lib/screen/ar.dart
+++ lib/screen/ar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
-
+import 'package:flutter_tts/flutter_tts.dart';
 class ARPage extends StatefulWidget {
   @override
   _ARPageState createState() => _ARPageState();
