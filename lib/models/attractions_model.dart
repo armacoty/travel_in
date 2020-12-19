2d9fc12e88c328d64d9a483114e3e7e2c8540d31
Vinogradov Alexander <astronom_35@mail.ru>
2020-12-19 10:12:23 +0300

    Update attractions_model.dart

--- lib/models/attractions_model.dart
+++ lib/models/attractions_model.dart
@@ -1,6 +1,9 @@
+// Достопримечательность
 class Attraction {
   final String name;
   final int id;
 
-  Attraction(this.name, this.id);
+  Map<String, String> questions = {};
+
+  Attraction(this.name, this.id, {this.questions});
 }
