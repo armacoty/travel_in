126b6e97183725ee9a0dc3d433b710b63d31e18b
Vinogradov Alexander <astronom_35@mail.ru>
2020-12-19 13:31:50 +0300

    Added more info to attraction

--- lib/models/attractions_model.dart
+++ lib/models/attractions_model.dart
@@ -2,11 +2,30 @@
 class Attraction {
   final String name;
   final int id;
+  String imageUrl;
+
+  double _rating = 1.0;
+
+  /// между 1.0 и 5.0 или Nan если еще нет рейтинга
+  set rating(double value) {
+    if (value <= 5.0 && value >= 1.0 || value.isNaN) {
+      _rating = value;
+    }
+  }
+
+  /// между 1.0 и 5.0 или Nan если еще нет рейтинга
+  double get rating => _rating;
 
   Map<String, String> questions = {};
 
   double latitude, longitude;
 
   Attraction(this.name, this.id,
-      {this.questions, this.latitude, this.longitude});
+      {this.questions,
+      this.latitude,
+      this.longitude,
+      this.imageUrl,
+      double rating = double.nan}) {
+    this.rating = rating;
+  }
 }
