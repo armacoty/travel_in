cb43e73c332ac3c38846075eb59dc4b6287d3172
Vinogradov Alexander <astronom_35@mail.ru>
2020-12-19 12:28:23 +0300

    Update client.dart

--- lib/client.dart
+++ lib/client.dart
@@ -2,8 +2,20 @@ import 'package:travel_in/models/attractions_model.dart';
 
 class Client {
   List<Attraction> getAttractions() {
-    // TODO: Remove mock data
-    return [];
+    // // TODO: Remove mock data
+    return [
+      Attraction(
+        "Успенский собор",
+        1,
+        questions: {
+          'Когда оно было построено?':
+              'Собор с колокольней и роскошными росписями, был построен в 2010 г. по образцу прежней церкви.',
+          'Какая у него высота?': '50 метров.',
+        },
+        latitude: 57.6223256680192,
+        longitude: 39.9018840690959,
+      ),
+    ];
   }
 }
 
