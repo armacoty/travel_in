7571546a8428254916fb4beec602ce89f1c77e7e
Vinogradov Alexander <astronom_35@mail.ru>
2020-12-19 10:24:02 +0300

    mock data

---
+++ lib/client.dart
@@ -0,0 +1,10 @@
+import 'package:travel_in/models/attractions_model.dart';
+
+class Client {
+  List<Attraction> getAttractions() {
+    // TODO: Remove mock data
+    return [];
+  }
+}
+
+Client client = new Client();
